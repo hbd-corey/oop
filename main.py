c51046f9fba7533a9a0ac3ec566131b00500e40f
Maciej Sitowski <maciej.sitowski@lingarogroup.com>
2021-11-02 12:51:19 +0100

    Add Employee class

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = f"{first}.{last}@company.com"
+
+
+    def fullname(self):
+        return f"{self.first} {self.last}"
+
+
+emp_1 = Employee("Johnny", "Bydlak", 50000)
+emp_2 = Employee("Test", "User", 60000)
+
+
+
 #
